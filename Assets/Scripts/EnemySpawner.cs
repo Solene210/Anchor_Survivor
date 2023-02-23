09f18edfab25ac3e8846d59aaacf9a78932f1aa8
Solene210 <jahn.solene@gmail.com>
2023-02-23 16:31:22 +0100

    Spawner amélioration

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,16 +8,32 @@ public class EnemySpawner : MonoBehaviour
     private GameObject _enemyPrefab;
     [SerializeField]
     private float _spawnInterval;
+    [SerializeField]
+    private float _spawnerRadius;
+    [SerializeField]
+    private Color _color;
+    [SerializeField]
+    private bool _drawGizmo;
 
     void Start()
     {
         StartCoroutine(SpawnEnemy(_spawnInterval, _enemyPrefab));
     }
 
+    private void OnDrawGizmos()
+    {
+        if (_drawGizmo)
+        {
+            Gizmos.color = _color;
+            Gizmos.DrawWireSphere(transform.position, _spawnerRadius);
+        }
+    }
     private IEnumerator SpawnEnemy(float interval, GameObject enemy)
     {
+        interval= Mathf.Max(_spawnInterval - Time.timeSinceLevelLoad * 0.05f, 0.1f);
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, transform.position, Quaternion.identity);
+        Vector2 position = Random.insideUnitCircle * _spawnerRadius + (Vector2)transform.position;
+        GameObject newEnemy = Instantiate(enemy, position, Quaternion.identity);
         StartCoroutine(SpawnEnemy(interval, enemy));
     }
 }
